0def19102826572e9be6bf87c67fa718c83d0325
Jimmy Luong <60137075+jimmyvluong@users.noreply.github.com>
2021-01-07 22:53:04 -0800

    Update airflow_examples.py

--- airflow_examples.py
+++ airflow_examples.py
@@ -1,5 +1,30 @@
 etl_dag = DAG(
   dag_id = 'etl_pipeline',
   default_args = {"start_date": "2020-01-08"}
+# Note that within any Python code, etl_dag is the variable identifier, but within the Airflow shell command, you must use the dag_id.
   
+# Running a simple Airflow task
+  # airflow run <dag_id> <task_id> <start_date>
+  airflow run example-etl download-file 2020-01-10
+  
+# DAGs on the command line
+  # list all DAGs
+  airflow list_dags
+  
+  # help
+  airflow -h
+  
+### EXAMPLE 1 ###  
+# Import the DAG object
+from airflow.models import DAG
+
+# Define the default_args dictionary
+default_args = {
+  'owner': 'dsmith',
+  'start_date': datetime(2020, 1, 14),
+  'retries': 2
+}
+
+# Instantiate the DAG object
+etl_dag = DAG('example_etl', default_args=default_args)
   
