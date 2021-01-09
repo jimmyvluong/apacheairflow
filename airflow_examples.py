1468a6d823520ef9b18a2bb27c644674f8377fc8
Jimmy Luong <60137075+jimmyvluong@users.noreply.github.com>
2021-01-08 18:02:17 -0800

    Update airflow_examples.py

--- airflow_examples.py
+++ airflow_examples.py
@@ -86,5 +86,32 @@ airflow webserver -p 9090
     bash_command = 'cat addresses.txt | awk "NF == 10" > cleaned.txt',
     dag = dag)
   
+  ## Example 5 ##
+  # A collection of 3 BashOperators in an Airflow workflow.
+  # This adds reliability and repeatablity to common tasks run from the shell.
+  
+  # Import the BashOperator
+  from airflow.operators.bash_operator import BashOperator
+
+  # Define the first BashOperator 
+    cleanup = BashOperator(
+      task_id='cleanup_task',
+      # Define the bash_command
+      bash_command='cleanup.sh',
+      # Add the task to the dag
+      dag= analytics_dag)
+  
+  # Define a second operator to run the `consolidate_data.sh` script
+    consolidate = BashOperator(
+      task_id='consolidate_task',
+      bash_command= 'consolidate_data.sh',
+      dag = analytics_dag)
+
+  # Define a final operator to execute the `push_data.sh` script
+  push_data = BashOperator(
+      task_id='pushdata_task',
+      bash_command='push_data.sh',
+      dag = analytics_dag)
+  
   
   
