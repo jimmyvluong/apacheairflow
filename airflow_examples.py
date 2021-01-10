4fe7bfcfa3d54be6a58da251b9bc035d50305ad1
Jimmy Luong <60137075+jimmyvluong@users.noreply.github.com>
2021-01-09 17:48:30 -0800

    Update airflow_examples.py

--- airflow_examples.py
+++ airflow_examples.py
@@ -229,6 +229,33 @@ task3 >> task1 # THIS LINE NEEDS TO BE DELETED.
     dag=process_sales_dag
 )
   
+  # Add another Python task
+  parse_file_task = PythonOperator(
+    task_id= 'parse_file',
+    # Set the function to call
+    python_callable = parse_file,
+    # Add the arguments
+    op_kwargs={'inputfile':'latestsales.json', 'outputfile':'parsedfile.json'},
+    # Add the DAG
+    dag=process_sales_dag
+)
+  
+  # Import the Operator
+  from airflow.operators.email_operator import EmailOperator
+
+  # Define the task
+  email_manager_task = EmailOperator(
+    task_id='email_manager',
+    to='manager@datacamp.com',
+    subject='Latest sales JSON',
+    html_content='Attached is the latest sales JSON file as requested.',
+    files='parsedfile.json',
+    dag=process_sales_dag
+)
+
+# Set the order of tasks
+pull_file_task >> parse_file_task >> email_manager_task
+  
 ### EmailOperator ###
   
   ## Email Example ##
